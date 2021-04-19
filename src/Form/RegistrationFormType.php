26858fd5b8de3e9a456799ca24e443540a7ca49b
Arnaud BODEL <arnaud@MacBook-Pro-de-Arnaud.local>
2021-04-19 15:56:36 +0200

    -

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +18,7 @@ use Symfony\Component\Validator\Constraints\GreaterThan;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Unique;
 
 class RegistrationFormType extends AbstractType
@@ -117,22 +119,70 @@ class RegistrationFormType extends AbstractType
             ])
 
             // Password
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+
+                // Pas de label sur le champ "repeated"
+                'label' => false,
+
+                'required' => true,
                 'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+
+                // Option du premier champ
+                'first_options' => [
+                    'label' => "Mot de passe",
+                    'attr' => [
+                        'placeholder' => "Saisir votre nouveau mot de passe",
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => "Nouveau mot de passe requis."
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => "Minimum 6 caractères",
+                            'max' => 32,
+                            'maxMessage' => "Maxi 32 caractères",
+                        ]),
+                        new Regex([
+                            'pattern' => "/^[a-z0-9]+/i",
+                            'message' => "doit contenir des caractères alphabétique et numerique uniquement"
+                        ])
+                    ]
                 ],
+
+                // Option du second champ
+                'second_options' => [
+                    'label' => "Confirmation du mot de passe",
+                    'attr' => [
+                        'placeholder' => "Répéter votre nouveau mot de passe",
+                    ]
+                ],
+
+                // Message d'erreur si les champs ne correspondent pas
+                'invalid_message' => "Les champs ne sont pas identique."
+
             ])
+
+
+
+
+            // ->add('plainPassword', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
         ;
     }
 
